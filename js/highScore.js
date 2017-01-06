93379fdb19faef47c4c60a947f9179dd24f8d7af
Dominic Serrano <dmsrojo@gmail.com>
2017-01-06 13:27:13 -0600

    Highscore logic now works

--- js/highScore.js
+++ js/highScore.js
@@ -1,27 +1,40 @@
 'use strict';
 
-function determineHighScore() {
+let gameOverText;
+let displayText;
 
-  let userObj = {
-    name: prompt('What is your name?'),
-    score: score
-  };
+
+function determineHighScore() {
 
   getAllUserScores()
   .then((scoreList) => {
 
+    // If players score is greater than the lowest score
+    // Add user to leaderboard and get scoreList again
     if (score > scoreList.slice(-1)[0].score) {
+      // Create user object with name and score
+      let userObj = {
+        name: prompt('What is your name?'),
+        score: score
+      };
+      // Post score obj to FB
       postCurrentUserScore(userObj)
       .then((status) => {
+        // Retrieve new scoreList
         getAllUserScores()
         .then((scoreList) => {
-          return `Game over, player score: ${score}\n
-          Press SPACE to play again.`
+          gameOverText = `Game over, player score: ${score}\n
+          Press SPACE to play again.`;
+          // Output leaderboard text
+          displayText = game.add.text(225, 250, gameOverText, { font: "30px Arial", fill:'#000', align: "center" });
         });
       });
     } else {
-      return `Sorry! Game over, player score: ${score}\n
-      Press SPACE to play again.`
+      gameOverText = `Sorry! Game over, player score: ${score}\n
+      Press SPACE to play again.`;
+
+      // Output leaderboard text
+      displayText = game.add.text(225, 250, gameOverText, { font: "30px Arial", fill:'#000', align: "center" });
     };
   });
 
--- states/creditMenu.js
+++ states/creditMenu.js
@@ -4,12 +4,9 @@ let creditMenu = {
   create: function() {
     game.stage.backgroundColor = '#CCC';
 
-    // let gameOverText = determineHighScore();
-    let gameOverText = `Game over, player score: ${score}\n
-    Press SPACE to play again.`;
-
-    let text = game.add.text(225, 250, gameOverText, { font: "30px Arial", fill:'#000', align: "center" });
-    text.inputEnabled = true;
+    // Determines if player is made a position on the leader board
+    // Then display text accordingly
+    determineHighScore();
 
     game.spaceBar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
