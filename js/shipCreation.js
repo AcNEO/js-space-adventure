329eb88158ab4fea77db224d6cca56edb57b7d0d
Dominic Serrano <dmsrojo@gmail.com>
2017-01-06 20:33:00 -0600

    Deleted speed test logs

--- js/shipCreation.js
+++ js/shipCreation.js
@@ -34,41 +34,33 @@ function outputShips() {
   } else if (shipCounter >= 25 && shipCounter < 50) {
     shipFleet.body.velocity.y = 400;
     shipFleet.body.velocity.x = game.rnd.between(-50, 50);
-    console.log("> 25");
     timeoutShips();
   } else if (shipCounter >= 50 && shipCounter < 100) {
     shipFleet.body.velocity.y = 500;
     shipFleet.body.velocity.x = game.rnd.between(-100, 100);
-    console.log("> 50");
     timeoutShips();
   } else if (shipCounter >= 100 && shipCounter < 150) {
     shipFleet.body.velocity.y = 600;
     shipFleet.body.velocity.x = game.rnd.between(-150, 150);
-    console.log("> 100");
     timeoutShips();
   } else if (shipCounter >= 150 && shipCounter < 200) {
     shipFleet.body.velocity.y = 700;
     shipFleet.body.velocity.x = game.rnd.between(-200, 200);
-    console.log("> 150");
     timeoutShips();
   } else if (shipCounter >= 200 && shipCounter < 250) {
     shipFleet.body.velocity.y = 800;
     shipFleet.body.velocity.x = game.rnd.between(-250, 250);
-    console.log("> 200");
     timeoutShips();
   } else if (shipCounter >= 250 && shipCounter < 300) {
     shipFleet.body.velocity.y = 900;
     shipFleet.body.velocity.x = game.rnd.between(-250, 250);
-    console.log("> 250");
     timeoutShips();
   } else if (shipCounter >= 300) {
     shipFleet.body.velocity.y = 1000;
     shipFleet.body.velocity.x = game.rnd.between(-300, 300);
-    console.log("> 300");
     timeoutShips();
   } else {
     shipFleet.body.velocity.y = 300;
-    console.log("< 25");
     timeoutShips();
   };
 
